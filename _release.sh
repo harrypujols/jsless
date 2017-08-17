16cc8641723cd187d2148777ffbb54dcb5469f6f
Harry Pujols <harrypujols@me.com>
2017-08-17 13:00:47 -0400

    Some force pushing

--- _release.sh
+++ _release.sh
@@ -4,6 +4,6 @@ TIMESTAMP=`date "+%Y%m%d-%H%M%S"`
 
 mkdir release
 cp _site/css/jsless.css release/jsless.css
-git add release && git commit -m "release $TIMESTAMP"
-git subtree push --prefix release origin release
+git add -f release && git commit -m "release $TIMESTAMP"
+git subtree push --prefix=release origin release
 rm -rf release
