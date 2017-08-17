a3a2d0b575745c80cf972aab6680b6f8e2809ac2
Harry Pujols <harrypujols@me.com>
2017-08-17 13:07:39 -0400

    Get back

--- _release.sh
+++ _release.sh
@@ -2,8 +2,5 @@
 
 TIMESTAMP=`date "+%Y%m%d-%H%M%S"`
 
-mkdir release
-cp _site/css/jsless.css release/jsless.css
-git add -f release && git commit -m "release $TIMESTAMP"
-git subtree push --prefix=release origin release
-rm -rf release
+git add -f _site/release && git commit -m "latest release $TIMESTAMP"
+git subtree push --prefix _site/release origin release
