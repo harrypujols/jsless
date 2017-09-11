6eac965a8128c4688e26c61b94cac95a735d4ea7
Harry Pujols <harrypujols@me.com>
2017-09-11 15:13:40 -0400

    modified release script

--- _release.sh
+++ _release.sh
@@ -5,7 +5,10 @@ TIMESTAMP=$(date +%s)
 jekyll build
 mkdir dist
 cp _site/release/* dist/
-git add dist && git commit -m "release #{$TIMESTAMP}"
+git add dist && git commit -m "push to release #{$TIMESTAMP}"
 git subtree push --prefix dist origin release
 # git push origin `git subtree split --prefix dist master`:release --force
 rm -rf dist
+git add .
+git commit -m "release completed #{$TIMESTAMP}"
+git push
