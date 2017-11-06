0fe5b6542ba1adc1164e66f0cae1efba7d65bfdd
Harry Pujols <harrypujols@me.com>
2017-11-06 12:41:35 -0500

    release completed #{1509990088}

--- _release.sh
+++ _release.sh
@@ -7,7 +7,7 @@ mkdir dist
 cp _site/release/* dist/
 git add dist && git commit -m "push to release #{$TIMESTAMP}"
 git subtree push --prefix dist origin release
-# git push origin `git subtree split --prefix dist master`:release --force
+git push origin `git subtree split --prefix dist master`:release --force
 rm -rf dist
 git add .
 git commit -m "release completed #{$TIMESTAMP}"
